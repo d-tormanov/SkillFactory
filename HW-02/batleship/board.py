8704ae12884ff8f7929356e6159834946c370de8
Denis Tormanov <dtormanov@gmail.com>
2024-10-23 19:41:37 +0300

    Added BatleShip game.

---
+++ HW-02/batleship/board.py
@@ -0,0 +1,109 @@
+from dot import Dot
+from exceptions import BoardOutException, BoardUsedException, BoardWrongShipException
+
+
+class Ship:
+    def __init__(self, bow, length, orientation):
+        self.bow = bow
+        self.length = length
+        self.orientation = orientation
+        self.lives = length
+
+    @property
+    def dots(self):
+        ship_dots = []
+        for i in range(self.length):
+            cur_x = self.bow.x
+            cur_y = self.bow.y
+
+            if self.orientation == 0:
+                cur_x += i
+            elif self.orientation == 1:
+                cur_y += i
+
+            ship_dots.append(Dot(cur_x, cur_y))
+
+        return ship_dots
+
+    def hit(self, shot):
+        return shot in self.dots
+
+
+class Board:
+    def __init__(self, hid=False, size=6):
+        self.size = size
+        self.hid = hid
+        self.count = 0
+        self.field = [["O"] * size for _ in range(size)]
+        self.busy = []
+        self.ships = []
+
+    def add_ship(self, ship):
+        for d in ship.dots:
+            if self.out(d) or d in self.busy:
+                raise BoardWrongShipException()
+        for d in ship.dots:
+            self.field[d.x][d.y] = "■"
+            self.busy.append(d)
+
+        self.ships.append(ship)
+        self.contour(ship)
+
+    def contour(self, ship, verb=False):
+        near = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 0), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        ]
+        for d in ship.dots:
+            for dx, dy in near:
+                cur = Dot(d.x + dx, d.y + dy)
+                if not (self.out(cur)) and cur not in self.busy:
+                    if verb:
+                        self.field[cur.x][cur.y] = "."
+                    self.busy.append(cur)
+
+    def out(self, d):
+        return not ((0 <= d.x < self.size) and (0 <= d.y < self.size))
+
+    def shot(self, d):
+        if self.out(d):
+            raise BoardOutException()
+
+        if d in self.busy:
+            raise BoardUsedException()
+
+        self.busy.append(d)
+
+        for ship in self.ships:
+            if d in ship.dots:
+                ship.lives -= 1
+                self.field[d.x][d.y] = "X"
+                if ship.lives == 0:
+                    self.count += 1
+                    self.contour(ship, verb=True)
+                    print("Корабль уничтожен!")
+                    return True
+                else:
+                    print("Корабль ранен!")
+                    return True
+
+        self.field[d.x][d.y] = "T"
+        print("Мимо!")
+        return False
+
+    def begin(self):
+        self.busy = []
+
+    def __str__(self):
+        res = ""
+        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, row in enumerate(self.field):
+            res += f"\n{i + 1} | " + " | ".join(row) + " |"
+
+        if self.hid:
+            res = res.replace("■", "O")
+        return res
+
+    def defeat(self):
+        return self.count == len(self.ships)
